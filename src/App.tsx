17e4919af7ec7160fd23529595da0914b8acdcc4
Khaled Silva <khaled.silva17@gmail.com>
2026-07-13 19:40:53 -0300

    feat: inicializa o projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <p className='text-red-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eum voluptas, doloremque ea repellendus dolorum soluta quis labore voluptate, cum iure excepturi ex ut repudiandae libero laudantium nesciunt earum consequuntur.</p>
+    </>
+  )
+}
+
+export default App
