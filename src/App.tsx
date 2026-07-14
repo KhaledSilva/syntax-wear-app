6da8fe9ab54a73dd159712703f7bb9008c0b181f
Khaled Silva <khaled.silva17@gmail.com>
2026-07-13 21:11:55 -0300

    feat: adiciona componente de header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header"
 
 
 function App() {
 
   return (
     <>
-      <p className='bg-error'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eum voluptas, doloremque ea repellendus dolorum soluta quis labore voluptate, cum iure excepturi ex ut repudiandae libero laudantium nesciunt earum consequuntur.</p>
+    <Header />
     </>
   )
 }
