8c09fefd203788078d6e111350a922e457d90d60
Khaled Silva <khaled.silva17@gmail.com>
2026-07-23 22:03:33 -0300

    feat: adiciona tela de nossas lojas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,13 @@
 import Logo from "@/assets/images/logo.png"
 import IconUser from "@/assets/images/icon-user.png"
 import IconAbout from "@/assets/images/icon-about.png"
-import IconCart from "@/assets/images/icon-cart.png"
 import { Link } from "@tanstack/react-router"
 import { ShoppingCart } from "../ShoppingCart"
 
 export const Header = () => {
     return (
         <div className="relative">
-            <header className="fixed top-10 left-0 right-0 z-10 mx-10">
+            <header className="fixed top-5 left-0 right-0 z-10 mx-10">
                 <div className="bg-white text-black max-w-[1320px] mx-auto flex justify-between items-center  py-3 px-7 rounded-2xl mt-5">
                     <Link to="/">
                     <img src={Logo} alt="Logo Syntaxwear" className="w-32 md:w-36" />
@@ -25,7 +24,7 @@ export const Header = () => {
                     <nav>
                         <ul className="flex gap-4 md:gap-10">
                             <li className="hidden md:block">
-                                <a href="#">Nossas lojas</a>
+                                <Link to="/our-stores">Nossa lojas</Link>
                             </li>
                             <li className="hidden md:block">
                                 <a href="#">Sobre</a>
